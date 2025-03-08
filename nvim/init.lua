fd2f186b8510d34ab71c14b2791b478e37496ed8
Mahmut <miftarosky@hotmail.com>
2025-03-08 17:24:20 +0100

    Updated NeoVim Config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,61 +1,37 @@
--- Set leader key to space (must be set before loading plugins)
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
-vim.g.maplocalleader = " " -- Optional: Set for filetype-specific mappings
 
--- Bootstrap Lazy.nvim package manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
--- Load plugins from specifications
-require("lazy").setup("plugins")
+local lazy_config = require "configs.lazy"
 
--- Basic settings
-vim.opt.number = true         -- Show line numbers
-vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.tabstop = 2          -- 2 spaces per tab
-vim.opt.shiftwidth = 2       -- 2 spaces per indent
-vim.opt.expandtab = true      -- Use spaces instead of tabs
-vim.opt.mouse = "a"          -- Enable mouse support
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
 
--- Plugin configurations
+  { import = "plugins" },
+}, lazy_config)
 
--- Catppuccin theme
-require("catppuccin").setup({
-  flavour = "mocha", -- latte, frappe, macchiato, mocha
-  transparent_background = false,
-})
-vim.cmd.colorscheme("catppuccin")
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
 
--- Lualine status line
-require("lualine").setup()
+require "options"
+require "nvchad.autocmds"
 
--- Nvim-tree file explorer
-require("nvim-tree").setup()
-vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
-
--- Telescope fuzzy finder
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files)
-vim.keymap.set("n", "<leader>fg", builtin.live_grep)
-
--- Auto pairs
-require("nvim-autopairs").setup()
-
--- Treesitter
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "lua", "python", "javascript", "typescript", "html", "css" },
-  highlight = { enable = true },
-})
-
--- Startup screen
-require("alpha").setup(require("alpha.themes.startify").config)
+vim.schedule(function()
+  require "mappings"
+end)
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,23 +1,25 @@
 return {
-  -- Colorscheme
-  { "catppuccin/nvim", name = "catppuccin" },
-  
-  -- Status line
-  { "nvim-lualine/lualine.nvim" },
-  
-  -- File tree
-  { "nvim-tree/nvim-tree.lua", dependencies = "nvim-tree/nvim-web-devicons" },
-  
-  -- Better syntax highlighting
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  
-  -- Fuzzy finder
-  { "nvim-telescope/telescope.nvim", dependencies = "nvim-lua/plenary.nvim" },
-  
-  -- Auto closing brackets/tags
-  { "windwp/nvim-autopairs" },
-  
-  -- Startup screen
-  { "goolord/alpha-nvim" },
-}
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
